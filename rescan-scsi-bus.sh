b810d340595c36f13750511e2311745cd3d6cd62
ankry <ankry@pld-linux.org>
2003-10-26 12:10:48 +0000

    - updated
    Changed files:
        rescan-scsi-bus.sh -> 1.3

--- rescan-scsi-bus.sh
+++ rescan-scsi-bus.sh
@@ -1,123 +1,104 @@
-#!/bin/sh
+#!/bin/bash
 # Skript to rescan SCSI bus, using the 
 # scsi add-single-device mechanism
-# (w) 98/03/19 Kurt Garloff <kurt@garloff.de> (c) GNU GPL
+# (w) 1998-03-19 Kurt Garloff <kurt@garloff.de> (c) GNU GPL
+# (w) 2003-07-16 Kurt Garloff <garloff@suse.de> (c) GNU GPL
+# $Id$
+
+setcolor ()
+{
+  red="\e[0;31m"
+  green="\e[0;32m"
+  yellow="\e[0;33m"
+  norm="\e[0;0m"
+}
+
+unsetcolor () 
+{
+  red=""; green=""
+  yellow=""; norm=""
+}
 
 # Return hosts. /proc/scsi/HOSTADAPTER/? must exist
 findhosts ()
 {
-	hosts=
-	for name in /proc/scsi/*/?; do
-		name=${name#/proc/scsi/}
-		if test ! $name = scsi ; then
-			hosts="$hosts ${name#*/}"
-			echo "Host adapter ${name#*/} (${name%/*}) found."
-		fi
-	done
+  hosts=
+  for driverdir in /proc/scsi/*; do
+    driver=${driverdir#/proc/scsi/}
+    [ $driver = scsi -o $driver = sg -o $driver = dummy ] && continue
+    for hostdir in $driverdir/*; do
+      name=${hostdir#/proc/scsi/*/}
+      [ $name = add_map -o $name = map -o $name = mod_parm ] && continue
+      num=$name
+      driverinfo=$driver
+      if [ -r $hostdir/status ]; then
+	num=$(printf '%d\n' `sed -n 's/SCSI host number://p' $hostdir/status`)
+	driverinfo="$driver:$name"
+      fi
+      hosts="$hosts $num"
+      echo "Host adapter $num ($driverinfo) found."
+    done
+  done
 }
 
 # Test if SCSI device $host $channen $id $lun exists
 # Outputs description from /proc/scsi/scsi, returns new
 testexist ()
 {
-	grepstr="scsi$host Channel: 0$channel Id: 0*$id Lun: 0$lun"
-	new=`cat /proc/scsi/scsi|grep -e"$grepstr"`
-	if test ! -z "$new" ; then
-		cat /proc/scsi/scsi|grep -e"$grepstr"
-		cat /proc/scsi/scsi|grep -A2 -e"$grepstr"|tail -2|pr -o4 -l1
-	fi
+  grepstr="scsi$host Channel: 0*$channel Id: 0*$id Lun: 0*$lun"
+  new=`cat /proc/scsi/scsi | grep -e"$grepstr"`
+  if test ! -z "$new"; then
+    cat /proc/scsi/scsi | grep -e"$grepstr"
+    cat /proc/scsi/scsi | grep -A2 -e"$grepstr" | tail -n2 | pr -o4 -l1
+  fi
 }
 
 # Perform search (scan $host)
 dosearch ()
 {
-    for channel in $channelsearch; do
-	for id in $idsearch; do
-	    for lun in $lunsearch; do
-		new=
-		devnr="$host $channel $id $lun"
-		echo "Scanning for device $devnr ..."
-		printf "OLD: "
-		testexist
-		if test ! -z "$remove" -a ! -z "$new" ; then
-		    echo "scsi remove-single-device $devnr" >/proc/scsi/scsi
-		    echo "scsi add-single-device $devnr" >/proc/scsi/scsi
-		    printf "\r\x1b[A\x1b[A\x1b[AOLD: "
-		    testexist
-		    if test -z "$new"; then
-			printf "\rDEL: \r\n\n\n\n";
-			let rmvd+=1;
-		    fi
-		fi
-		if test -z "$new" ; then
-		    printf "\rNEW: "
-		    echo "scsi add-single-device $devnr" >/proc/scsi/scsi
-		    testexist
-		    if test -z "$new"; then
-			printf "\r\x1b[A"
-		    else
-			let found+=1;
-		    fi
-		fi
-	    done
-	done
+  for channel in $channelsearch; do
+    for id in $idsearch; do
+      for lun in $lunsearch; do
+        new=
+	devnr="$host $channel $id $lun"
+	echo "Scanning for device $devnr ..."
+	printf "${yellow}OLD: $norm"
+	testexist
+	if test ! -z "$remove" -a ! -z "$new"; then
+	  # Device exists and we're in remove mode, so remove and readd
+	  echo "scsi remove-single-device $devnr" >/proc/scsi/scsi
+	  echo "scsi add-single-device $devnr" >/proc/scsi/scsi
+	  printf "\r\x1b[A\x1b[A\x1b[A${yellow}OLD: $norm"
+	  testexist
+	  if test -z "$new"; then 
+	    printf "\r${red}DEL: $norm\r\n\n\n\n"; let rmvd+=1; 
+          fi
+	fi
+	if test -z "$new"; then
+	  # Device does not exist, try to add
+	  printf "\r${green}NEW: $norm"
+	  echo "scsi add-single-device $devnr" >/proc/scsi/scsi
+	  testexist
+	  if test -z "$new"; then
+	    # Device not present
+	    printf "\r\x1b[A";
+  	    # Optimization: if lun==0, stop here (only if in non-remove mode)
+	    if test $lun = 0 -a -z "$remove" -a $optscan = 1; then 
+	      break;
+	    fi  
+	  else 
+	    let found+=1; 
+	  fi
+	fi
+      done
     done
+  done
 }
-	  
-  
+ 
 # main
-
-if [ ! -e /proc/scsi ]; then
-	echo "You don't have SCSI, go away!"
-	exit 1
-fi
-
-if [ "$1" = "--help" -o "$1" = "-h" ]; then 
-	echo "Usage: rescan-scsi-bus.sh [-l] [-w] [-c] [host [host ...]]"
-	echo " -l	activates scanning for LUNs 0..7	[default: 0]"
-	echo " -w	enables scanning for device IDs 0..15	[def.: 0..7]"
-	echo " -r	enables removing of devices		[default: disabled]"
-	echo " -c	enables scanning of channels 0 1	[default: 0]"
-	echo " If hosts are given, only these are scanned	[default: all]"
-	exit 0
-fi
-
-# defaults
-lunsearch="0"
-idsearch="0 1 2 3 4 5 6 7"
-channelsearch="0"
-remove=""
-
-# Scan options
-opt="$1"
-while [ ! -z "$opt" -a -z "${opt##-*}" ]; do
-	opt=${opt#-}
-	case "$opt" in
-	    l) lunsearch="0 1 2 3 4 5 6 7" ;;
-	    w) idsearch="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15" ;;
-	    c) channelsearch="0 1" ;;
-	    r) remove=1 ;;
-	    *) echo "Unknown option -$opt !" ;;
-	esac
-	shift
-	opt="$1"
-done    
-
-# Hosts given ?
-if [ -z $1 ]; then
-	findhosts
-else
-	hosts=$*
-fi
-
-declare -i found=0
-declare -i rmvd=0
-for host in $hosts; do
-	dosearch
-done
-echo "$found new device(s) found."
-echo "$rmvd device(s) removed."
- echo "Options:"
+if test @$1 = @--help -o @$1 = @-h -o @$1 = @-?; then
+    echo "Usage: rescan-scsi-bus.sh [options] [host [host ...]]"
+    echo "Options:"
     echo " -l activates scanning for LUNs 0-7    [default: 0]"
     echo " -w scan for target device IDs 0 .. 15 [default: 0-7]"
     echo " -c enables scanning of channels 0 1   [default: 0]"
